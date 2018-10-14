548abcf5784c744a492f06af62066d94c3d8f3f9
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2018-10-14 19:13:11 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,40 @@ import HelloWorld from './components/HelloComponent'
 import Counter from './components/CounterComponent'
 
 class App extends Component {
+
+  state = {
+    incrementarPor: 1,
+    decrementarPor: 1
+  }
+
+  _handleChangeIncrementarPor = event => {
+    this.setState({
+      incrementarPor: parseInt(event.target.value)
+    })
+  }
+
+  _handleChangeDecrementarPor = event => {
+    this.setState({
+      decrementarPor: parseInt(event.target.value)
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <HelloWorld name="World" texto="Meu primeiro exemplo com props." />
-        <Counter incrementarPor={2} decrementarPor={1} />
+        
+        <input type="text" 
+          name="incrementarPor" 
+          value={this.state.incrementarPor} 
+          onChange={this._handleChangeIncrementarPor} />
+          
+        <input type="text" 
+          name="decrementarPor" 
+          value={this.state.decrementarPor} 
+          onChange={this._handleChangeDecrementarPor} />
+
+        <Counter incrementarPor={this.state.incrementarPor} decrementarPor={this.state.decrementarPor} />
       </div>
     );
   }
